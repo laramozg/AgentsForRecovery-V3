2702df00ce9ad0c93257a248f38d9bc6d530b57c
Larisa Mozgovaya <larisa.mont73@gmail.com>
2024-11-01 21:17:12 +0300

    create initial settings

--- sports-user/src/main/java/org/example/sportsuser/services/AuthService.java
+++ sports-user/src/main/java/org/example/sportsuser/services/AuthService.java
@@ -1,7 +1,5 @@
 package org.example.sportsuser.services;
 
 public class AuthService {
-    private final UserService userService;
-    private final PasswordEncoder encoder;
-    private final AuthenticationProvider authProvider;
+
 }
