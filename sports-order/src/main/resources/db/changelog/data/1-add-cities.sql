535e20e871cb87ddad02e12614ef03bbc398d159
Larisa Mozgovaya <larisa.mont73@gmail.com>
2024-11-19 16:45:38 +0300

    feat: adding migrations

---
+++ sports-order/src/main/resources/db/changelog/data/1-add-cities.sql
@@ -0,0 +1,7 @@
+insert into cities (id,
+                    name,
+                    region)
+
+values ('New York', 'New York State'),
+       ('Los Angeles', 'California'),
+       ('Chicago', 'Illinois');
---
+++ sports-order/src/main/resources/db/changelog/data/3-add-mutilations.sql
@@ -0,0 +1,6 @@
+insert into mutilations (id,
+                         type,
+                         price)
+values ('To break an arm', 15000),
+       ('To break a leg', 15000),
+       ('To beat up', 20000);
